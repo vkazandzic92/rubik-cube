94fb17e302a7a08286207e4f9bbf496eb8541001
Kazandzic, Vladimir (FI4_EXTERN) <net.vladimir.kazandzic@mhp.com>
2022-05-22 12:56:29 +0200

    Added java project

---
+++ src/test/java/com/eqs/rubik/cube/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eqs.rubik.cube;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
